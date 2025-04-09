633a0d27eb3fa10c9379ae33833450a138dbc988
William Roy <wroy@proton.me>
2025-04-09 14:11:27 -0400

    Compile CLI

--- lysis/lysis/lysis.cxx
+++ lysis/lysis/lysis.cxx
@@ -1,15 +1,63 @@
 #include <iostream>
 
+#include <lysis/version.hxx>
+#include <lysis/options.hxx>
+
+using namespace std;
+using namespace lysis;
+
+namespace lysis
+{
+  namespace
+  {
+    void
+    usage (ostream& os)
+    {
+      os << "usage: driver [options] <names>" << endl << "options:" << endl;
+      options::print_usage (os);
+    }
+  }
+}
+
 int
 main (int argc, char* argv[])
 {
-  using namespace std;
-
-  if (argc < 2)
+  try
   {
-    cerr << "error: missing name" << endl;
+    options o (argc, argv);
+
+    if (o.build2_metadata_specified ())
+    {
+      auto& o (cout);
+
+      // Note that the export.metadata variable should be the first non-
+      // blank/comment line.
+      //
+      o << "# build2 buildfile lysis"                                  << endl
+        << "export.metadata = 1 lysis"                                 << endl
+        << "lysis.name = [string] lysis"                               << endl
+        << "lysis.version = [string] '"  << LYSIS_VERSION_FULL << '\'' << endl
+        << "lysis.checksum = [string] '" << LYSIS_VERSION_FULL << '\'' << endl;
+
+      return 0;
+    }
+
+    if (o.help ())
+    {
+      usage (cout);
+      return 0;
+    }
+
+    if (o.version ())
+    {
+      cout << "Lysis " << LYSIS_VERSION_ID << endl;
+      return 0;
+    }
+  }
+  catch (const cli::exception& e)
+  {
+    cerr << e << endl;
+    usage (cerr);
     return 1;
   }
-
-  cout << "Hello, " << argv[1] << '!' << endl;
 }
